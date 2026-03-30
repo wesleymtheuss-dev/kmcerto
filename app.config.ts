5c5240360d0392880a0c5e5f4b6905e6279c4f2c
wesleymtheuss-dev <wesleymtheuss@gmail.com>
2026-03-29 23:58:00 -0300

    app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -21,7 +21,7 @@ const config: ExpoConfig = {
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
   userInterfaceStyle: "automatic",
-  newArchEnabled: false,
+  newArchEnabled: true,
   ios: {
     supportsTablet: true,
     bundleIdentifier: env.iosBundleId,
